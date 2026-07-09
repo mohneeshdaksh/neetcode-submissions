b0ff73872663b0d4873d61ba9696b05da843b109
Mohneesh Daksh <149918840+mohneeshdaksh@users.noreply.github.com>
2026-07-08 19:32:49 -0700

    Add: decode-string - submission-2

---
+++ Data Structures & Algorithms/decode-string/submission-2.py
@@ -0,0 +1,18 @@
+class Solution:
+    def decodeString(self, s: str) -> str:
+        nums = ["0","1","2","3","4","5","6","7","8","9"]
+        stack = []
+        for i in s:
+            if i == "]":
+                st = ""
+                mul = ""
+                while stack[-1] != "[":
+                    st = stack.pop() + st
+                stack.pop()
+                while stack and stack[-1] in nums:
+                    mul += stack.pop()
+                mul = "".join(reversed(mul))
+                stack.append(st * int(mul))
+            else:
+                stack.append(i)
+        return "".join(stack)
